f2c93047faa734c7b57c2fa993b0f19a2dd7af15
unknown <>
2016-11-06 08:01:53 +0900

    testクラスを画面遷移とBluetooth接続で分解。

--- Flexible Keyboard at Unity/Assets/Resources/Keys/Key.cs
+++ Flexible Keyboard at Unity/Assets/Resources/Keys/Key.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Key : MonoBehaviour {
+    private int ONE_GRID = 25;
 
     public string key = "";
+    public Vector2 size; 
+
+    public void Set(string key, Vector2 size, Vector2 potition)
+    {
+        this.key = key;
+        name = key;
+        this.size = size;
+        RectTransform RT = GetComponent<RectTransform>();
+        RT.sizeDelta = new Vector2(size.x * ONE_GRID, size.y * ONE_GRID);
+        RT.anchoredPosition = potition;
+    }
 
     public void send()
     {
         if (key != "")
         {
-            Debug.Log("sendしたがるよ");
             if (!Bluetooth.TrySend(key))
                 Error.error("sendできんかったで。(keyのやつ)");
         }
---
+++ Flexible Keyboard at Unity/Assets/Resources/Scripts/MoveScreen.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScreen : MonoBehaviour {
+    RectTransform[] rectTransforms;
+    RectTransform B;
+    RectTransform C;
+    RectTransform D;
+    RectTransform E;
+
+    void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Init()
+    {
+        rectTransforms = new RectTransform[5];
+        for(int i = 0; i < 5; i++)
+            rectTransforms[i] = transform.GetChild(i).GetComponent<RectTransform>();
+    }
+
+    // x移動
+    void MoveX(int x)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            RectTransform A = rectTransforms[i];
+            A.anchoredPosition = new Vector2(A.anchoredPosition.x + x, A.anchoredPosition.y);
+        }
+    }
+
+    // y移動
+    void MoveY(int y)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            RectTransform A = rectTransforms[i];
+            A.anchoredPosition = new Vector2(A.anchoredPosition.x, A.anchoredPosition.y + y);
+        }
+    }
+
+    void MovingY(int num)
+    {
+        StartCoroutine(MovingY_Animation(num));
+    }
+
+    IEnumerator MovingY_Animation(int num)
+    {
+        int sign = num / Mathf.Abs(num);
+        for (int i = 0; i < 20 * Mathf.Abs(num); i++)
+        {
+            MoveY(20 * sign);
+            yield return null;
+        }
+    }
+}
