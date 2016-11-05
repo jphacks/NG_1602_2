d4a8c729f131b234f3bf696f41e25d4c34843de0
unknown <>
2016-11-06 06:46:41 +0900

    画面移動の準備。キーボードのサンプル。

---
+++ Flexible Keyboard at Unity/Assets/Resources/Keys/Key.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+    public string key = "";
+
+    public void send()
+    {
+        if (key != "")
+        {
+            Debug.Log("sendしたがるよ");
+            if (!Bluetooth.TrySend(key))
+                Error.error("sendできんかったで。(keyのやつ)");
+        }
+    }
+}
--- Flexible Keyboard at Unity/Assets/Resources/Scripts/Test.cs
+++ Flexible Keyboard at Unity/Assets/Resources/Scripts/Test.cs
@@ -6,6 +6,37 @@ using System.Collections.Generic;
 public class Test : MonoBehaviour {
     private List<string[]> deviceList = null;
 
+    RectTransform A;
+    RectTransform B;
+    RectTransform C;
+    RectTransform D;
+    RectTransform E;
+    void Init()
+    {
+        Transform Pparent = transform.parent.parent;
+        A = Pparent.GetChild(0).GetComponent<RectTransform>();
+        B = Pparent.GetChild(1).GetComponent<RectTransform>();
+        C = Pparent.GetChild(2).GetComponent<RectTransform>();
+        D = Pparent.GetChild(3).GetComponent<RectTransform>();
+        E = Pparent.GetChild(4).GetComponent<RectTransform>();
+    }
+    void addAnPoX(int x)
+    {
+        A.anchoredPosition = new Vector2(A.anchoredPosition.x + x, A.anchoredPosition.y);
+        B.anchoredPosition = new Vector2(B.anchoredPosition.x + x, B.anchoredPosition.y);
+        C.anchoredPosition = new Vector2(C.anchoredPosition.x + x, C.anchoredPosition.y);
+        D.anchoredPosition = new Vector2(D.anchoredPosition.x + x, D.anchoredPosition.y);
+        E.anchoredPosition = new Vector2(E.anchoredPosition.x + x, E.anchoredPosition.y);
+    }
+    void addAnPoY(int y)
+    {
+        A.anchoredPosition = new Vector2(A.anchoredPosition.x, A.anchoredPosition.y + y);
+        B.anchoredPosition = new Vector2(B.anchoredPosition.x, B.anchoredPosition.y + y);
+        C.anchoredPosition = new Vector2(C.anchoredPosition.x, C.anchoredPosition.y + y);
+        D.anchoredPosition = new Vector2(D.anchoredPosition.x, D.anchoredPosition.y + y);
+        E.anchoredPosition = new Vector2(E.anchoredPosition.x, E.anchoredPosition.y + y);
+    }
+
     public void test()
     {
         if (deviceList != null) Error.error("このボタンは2回押されるとこまるわ(´・ω・)");
@@ -49,10 +80,12 @@ public class Test : MonoBehaviour {
         Debug.Log("try : " + tryresult);
         if (tryresult)
         {
-            GameObject myButton = Resources.Load<GameObject>("MyButton/Prefab");
-            GameObject newButton = Instantiate(myButton, new Vector2(0, 50), Quaternion.identity) as GameObject;
-            newButton.transform.SetParent(transform.parent, false);
-            newButton.GetComponent<MyButton>().Set(gameObject, "send");
+            Init();
+            addAnPoY(-400);
+//            GameObject myButton = Resources.Load<GameObject>("MyButton/Prefab");
+//            GameObject newButton = Instantiate(myButton, new Vector2(0, 50), Quaternion.identity) as GameObject;
+//            newButton.transform.SetParent(transform.parent, false);
+//            newButton.GetComponent<MyButton>().Set(gameObject, "send");
         }
         else
         {
